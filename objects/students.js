0ab0c1d4e56d36eb0fa56bfd31487e7970610fc5
alejogs4 <alejogs4@gmail.com>
2019-07-21 13:35:18 -0500

    Agrega archivos iniciales

---
+++ objects/students.js
@@ -0,0 +1,39 @@
+const students = [
+  {
+    id: 1,
+    name: 'Santiago',
+    age: 22,
+    active: true,
+    subjects: ['Animacion', 'Diseño']
+  },
+  {
+    id: 2,
+    name: 'Jhoan',
+    age: 17,
+    active: true,
+    subjects: ['Contabilidad', 'Administracion']
+  },
+  {
+    id: 3,
+    name: 'Laura',
+    age: 23,
+    active: false,
+    subjects: ['Matematicas', 'Fundamentos']
+  },
+  {
+    id: 4,
+    name: 'Miguel',
+    age: 21,
+    active: true,
+    subjects: ['Matematicas', 'POO']
+  },
+  {
+    id: 5,
+    name: 'Karoline',
+    age: 20,
+    active: true,
+    subjects: ['Toma de decisiones', 'Animacion']
+  },
+]
+
+module.exports = students
