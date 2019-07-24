20a5c5afb985f3c0e95710562f6a43e91dc49156
alejogs4 <alejogs4@gmail.com>
2019-07-24 17:07:47 -0500

    Agrega ejercicio de ajax

--- ajax/main.js
+++ ajax/main.js
@@ -1,1 +1,16 @@
 const USERS_URL = 'https://jsonplaceholder.typicode.com/users'
+const button = document.querySelector('.button')
+const list = document.querySelector('.users')
+
+button.addEventListener('click', function() {
+  fetch(USERS_URL)
+    .then((response) => {
+      return response.json()
+    })
+    .then(info => {
+      list.innerHTML = ''
+      info.forEach(user => {
+        list.innerHTML += `<li>${user.name}</li>`
+      })
+    })
+})
